e977803920b0c50e9e7e7516805956659d191f26
Kilumkothn <mpjy000@gmail.com>
2024-07-22 11:22:15 +0800

    Update speedtest-cli-ubuntu.sh

--- depoly codes/speedtest-cli-ubuntu.sh
+++ depoly codes/speedtest-cli-ubuntu.sh
@@ -2,11 +2,6 @@
 echo "this command by speedtest offical website:https://www.speedtest.net/apps/cli"
 echo "The author is not responsible for any consequences of using this code."
 read -p "please input enter start install." a
-echo "uninstalling speedtest."
-sudo rm /etc/apt/sources.list.d/speedtest.list
-sudo apt-get update
-sudo apt-get remove speedtest
-sudo apt-get remove speedtest-cli
 echo "installing speedtest."
 sudo apt-get install curl
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
