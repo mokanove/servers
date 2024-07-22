b04dedc52a80fdced17f08d5250fc1a2ff1bfc50
Kilumkothn <mpjy000@gmail.com>
2024-07-22 11:16:06 +0800

    Update proxy-cloudflare-IPs.sh

--- depoly codes/proxy-cloudflare-IPs.sh
+++ depoly codes/proxy-cloudflare-IPs.sh
@@ -3,6 +3,7 @@ echo "Do you want do this? It will you 80 443 2082 2052 8080 port is opened."
 echo "If it can't use, please change "104.20.182.94" to others cloudflare offical IPs."
 echo "The author is not responsible for any consequences of using this code."
 read -p "please input enter start install." a
+sudo apt update && sudo apt upgrade -y && sudo apt install iptables
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -F
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 104.20.182.94:80
