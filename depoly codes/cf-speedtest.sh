4a5c61b01f97e75d16fb3636ae24472de8cb6900
Kilumkothn <mpjy000@gmail.com>
2024-09-09 09:49:14 +0800

    Add files via upload

---
+++ depoly codes/cf-speedtest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "The project by XIU2:https://github.com/XIU2/CloudflareSpeedTest"
+echo "The first IP is best Cloudflare IP for this server."
+echo "The author is not responsible for any consequences of using this code."
+read -p "please input enter start install." a
+wget https://github.com/Kilumkothn/IPs/raw/main/depoly%20codes/CloudflareST
+wget https://github.com/Kilumkothn/IPs/blob/main/CDNIPs/Cloudflare.txt -O ip.txt
+chmod 777 CloudflareST
+./CloudflareST
+echo "All Done!"
