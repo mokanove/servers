4ec0166c3fbbe099b6d66285ba98cc2e6506d10b
Kilumkothn <mpjy000@gmail.com>
2024-09-15 16:41:57 +0800

    Update cf-speedtest.sh

--- depoly codes/cf-speedtest.sh
+++ depoly codes/cf-speedtest.sh
@@ -7,4 +7,6 @@ wget https://github.com/Kilumkothn/IPs/raw/main/depoly%20codes/CloudflareST
 wget https://s.867678.xyz/ip.txt
 chmod 777 CloudflareST
 ./CloudflareST
+echo "Your best Cloudflare IP is "$(awk -F',' 'NR==2 {print $1}' result.csv)
+rm result.csv
 echo "All Done!"
