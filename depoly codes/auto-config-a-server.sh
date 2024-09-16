b5c1eeccab739c696d944493cbc9322145f69590
Kilumkothn <mpjy000@gmail.com>
2024-09-16 10:19:01 +0800

    Update auto-config-a-server.sh

--- depoly codes/auto-config-a-server.sh
+++ depoly codes/auto-config-a-server.sh
@@ -4,6 +4,7 @@ echo "It will be off the firewall !"
 echo "The project by https://github.com/sjlleo/netflix-verify/ and https://github.com/XIU2/CloudflareSpeedTest and https://www.speedtest.net/apps/cli"
 echo "The author is not responsible for any consequences of using this code."
 read -p "please input enter start install." a
+
 sudo apt update 
 sudo apt upgrade -y
 ufw disable
