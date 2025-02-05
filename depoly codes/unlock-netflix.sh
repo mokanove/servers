cc4e6efd9c686e1e1a2db7295d65429c1785797f
Saŕmioè <mpjy000@gmail.com>
2025-02-05 13:05:51 +0800

    Update unlock-netflix.sh

--- depoly codes/unlock-netflix.sh
+++ depoly codes/unlock-netflix.sh
@@ -9,6 +9,7 @@ curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmo
 echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 sudo apt-get update && sudo apt-get install cloudflare-warp -y
 read -p "You need agree the lisense to next , please input enter and next." b
-warp-cli registration new -y
-warp-cli mode proxy&&warp-cli connect
+warp-cli registration new
+warp-cli mode proxy
+warp-cli connect
 rm unlock-netflix.sh
